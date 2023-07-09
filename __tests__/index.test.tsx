889af7aa180cbcb2fc980117fbb43ffe5f5c9aca
marcu <marcussoh38@gmail.com>
2023-07-09 16:59:34 +0800

    added more tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,33 +3,64 @@ import { UserContext } from "@/app/providers";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
+import USER from "../__mocks__/user.json";
+import QUIZZES from "../__mocks__/quizzes.json";
+import { act } from "react-dom/test-utils";
+
+const getQuizData = jest.fn(() =>
+    Promise.resolve({
+        json: () => Promise.resolve({ data: QUIZZES }),
+    })
+) as jest.Mock;
+global.fetch = getQuizData;
+
 describe("Home page", () => {
-    it("should render a not-logged in home page", () => {
+    it("should render a not-logged in home page", async () => {
         render(<HomePage />);
 
-        const getStartedBtn = screen.getByTestId("cta-btn");
+        const getStartedBtn = await screen.findByTestId("cta-btn");
         expect(getStartedBtn).toBeInTheDocument();
     });
 
-    it("should render a logged-in home page", () => {
-        const mockUser = {
-            canvasApiToken: "",
-            displayName: "test",
-            email: "",
-            photoURL: "",
-            uid: "",
-            uploadedIds: [],
-        };
-        render(
-            <UserContext.Provider
-                value={{
-                    user: mockUser,
-                }}
-            >
-                <HomePage />
-            </UserContext.Provider>
+    it("should render a logged-in home page", async () => {
+        act(() =>
+            render(
+                <UserContext.Provider
+                    value={{
+                        user: USER,
+                    }}
+                >
+                    <HomePage />
+                </UserContext.Provider>
+            )
         );
 
-        expect(screen.getByTestId("add-new-btn")).toBeInTheDocument();
+        expect(await screen.findByTestId("add-new-btn")).toBeInTheDocument();
+    });
+
+    it("should get and display user data", async () => {
+        await act(() =>
+            render(
+                <UserContext.Provider
+                    value={{
+                        user: USER,
+                    }}
+                >
+                    <HomePage />
+                </UserContext.Provider>
+            )
+        );
+
+        // Fetch server with database ID
+        expect(getQuizData).toHaveBeenCalledWith("/api/?uid=1234567890");
+
+        // expect the homepage to display the title of the course, in this case
+        // SOCT101 SoC Teaching Workshop
+        expect(
+            await screen.findByText(/SOCT101 SoC Teaching Workshop/)
+        ).toBeInTheDocument();
+
+        // expect there to only be one card with two attempts
+        expect(await screen.findByText(/Attempt #2/)).toBeInTheDocument();
     });
 });
