a2be2d04bb176ca2df2a6ec96496e114c69e6fcc
Justin <justinlim60@gmail.com>
2023-08-06 18:22:43 +0800

    login component render test

--- __tests__/components/Auth/LoginComponent.test.tsx
+++ __tests__/components/Auth/LoginComponent.test.tsx
@@ -1,5 +1,11 @@
 import "@testing-library/jest-dom";
-import { render, fireEvent, waitFor, screen, renderHook } from '@testing-library/react';
+import {
+    render,
+    fireEvent,
+    waitFor,
+    screen,
+    renderHook,
+} from "@testing-library/react";
 import { useRouter } from "next/router";
 import LoginComponent from "@/components/Auth/LoginComponent";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -16,23 +22,21 @@ jest.mock("next/navigation", () => ({
     },
     useParams: () => ({ quizUploadId: "CgBnvGYEayotCQXfrpi7" }),
 }));
-describe('LoginComponent', () => {
-    it('should render the login form initially', () => {
-render(
-    <UserContext.Provider value={{ user: false, setUser: jest.fn() }}>
-<AppRouterContextProviderMock router={{}}>
-    <ChakraProvider theme = {customTheme}>
-        <LoginComponent />
-    </ChakraProvider>
-</AppRouterContextProviderMock>
-</UserContext.Provider>)
-            
-      // Assert that the login form is rendered
-      expect(screen.getByText('Do we know you?')).toBeInTheDocument();
-      expect(screen.getByText('Sign in')).toBeInTheDocument();
-      expect(screen.getByLabelText('Email')).toBeInTheDocument();
-      expect(screen.getByLabelText('Password')).toBeInTheDocument();
-      expect(screen.getByText('Forgot password?')).toBeInTheDocument();
+describe("LoginComponent", () => {
+    it("should render the login form initially", () => {
+        render(
+            <UserContext.Provider value={{ user: false, setUser: jest.fn() }}>
+                <AppRouterContextProviderMock router={{}}>
+                    <ChakraProvider theme={customTheme}>
+                        <LoginComponent />
+                    </ChakraProvider>
+                </AppRouterContextProviderMock>
+            </UserContext.Provider>
+        );
+
+        // Assert that the login form is rendered
+        expect(screen.getByText("Do we know you?")).toBeInTheDocument();
+        expect(screen.getByText("Sign in")).toBeInTheDocument();
+        expect(screen.getByText("Forgot password?")).toBeInTheDocument();
     });
-}
-)
+});
